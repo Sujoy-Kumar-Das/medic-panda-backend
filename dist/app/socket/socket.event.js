69fe8a54c27fbcecb000db4482ddd39704893eeb
Sujoy-Kumar-Das <sujoykumardas75@gmial.com>
2026-07-30 13:06:36 +0600

    code base full  updated

--- dist/app/socket/socket.event.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.socketEvent = void 0;
-exports.socketEvent = {
-    order: 'order',
-    cart: 'cart',
-    product: 'product',
-    connection: 'connection',
-};
--- dist/app/utils/generateTransactionId.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generateTransactionId = () => {
-    const timestamp = Date.now();
-    const random = Math.floor(Math.random() * 1000000);
-    return `${timestamp}${random}`;
-};
-exports.default = generateTransactionId;
